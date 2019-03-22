bbfef7787eddbaa71a7f95d4efd863d9615f44e0
Zoltán Herczog <zoltan.herczog@cheppers.com>
2019-03-22 17:39:15 +0100

    Refactor DeprecationDashboardController

--- src/Controller/DeprecationDasboardController.php
+++ src/Controller/DeprecationDasboardController.php
@@ -45,41 +45,15 @@ class DeprecationDasboardController extends ControllerBase {
     $content['form'] = $this->formBuilder()->doBuildForm('drupal_readiness_form', $content, $formState);
 
     $projects = $this->projectCollector->collectProjects();
-
-    $custom = [];
-    foreach ($projects['custom'] as $projectName => $projectData) {
-      $cache = $this->cache->get($projectName);
-      $info = $projectData->info;
-      $custom[$projectName] = [
-        'project' => $info['name'],
-        'status' => $cache->data ?? [],
-      ];
-    }
-
-    $contrib = [];
-    foreach ($projects['contrib'] as $projectName => $projectData) {
-      $cache = $this->cache->get($projectName);
-      $info = $projectData->info;
-      $contrib[$projectName] = [
-        'project' => $info['name'],
-        'status' => $cache->data ?? [],
-      ];
-    }
+    $custom = $this->buildProjectRows($projects['custom']);
+    $contrib = $this->buildProjectRows($projects['contrib']);
 
     $content['drupal_readiness_form']['custom'] = [
       '#type' => 'details',
       '#title' => $this->t('Custom modules and themes'),
       '#tree' => TRUE,
       '#open' => TRUE,
-      'data' => [
-        '#type' => 'table',
-        '#header' => [
-          $this->t('Project'),
-          $this->t('Status'),
-          $this->t('Operations'),
-        ],
-        '#rows' => $custom,
-      ],
+      'data' => $custom,
     ];
 
     $content['drupal_readiness_form']['contrib'] = [
@@ -87,19 +61,98 @@ class DeprecationDasboardController extends ControllerBase {
       '#title' => $this->t('Contributed modules and themes'),
       '#tree' => TRUE,
       '#open' => TRUE,
+      'data' => $contrib,
+    ];
+
+    return $content;
+  }
+
+  protected function buildProjectRows(array $projects) {
+    $notDeprecated = [];
+    $deprecated = [];
+    $notScanned = [];
+    $projectsDisplay = [];
+    $totalErrors = 0;
+
+    foreach ($projects as $projectName => $projectData) {
+      $cache = $this->cache->get($projectName);
+      $info = $projectData->info;
+
+      if (empty($cache)) {
+        $notScanned[] = [
+          'project' => $info['name'],
+        ];
+        continue;
+      }
+
+      $deprecationReportRaw = $cache->data;
+      $deprecationReport = json_decode($deprecationReportRaw);
+      $errors = $deprecationReport->totals->file_errors;
+
+      if ($errors === 0) {
+        $notDeprecated[] = [
+          'project' => $info['name'],
+        ];
+        continue;
+      }
+      $totalErrors += $errors;
+
+      if (isset($info['version'])) {
+        $deprecated[] = [
+          'project' => $info['name'] . $info['version'],
+          'status' => $this->t('@errors errors', ['@errors' => $errors]),
+        ];
+        continue;
+      }
+
+      $deprecated[] = [
+        'project' => $info['name'],
+        'status' => $this->t('@errors errors', ['@errors' => $errors]),
+      ];
+    }
+
+    $projectsDisplay['deprecated'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Found @project project with @errorCount known drupal 9 compatibility errors', ['@project' => count($deprecated), '@errorCount' => $totalErrors]),
+      '#tree' => TRUE,
+      '#open' => !empty($deprecated) ? TRUE : FALSE,
       'data' => [
         '#type' => 'table',
         '#header' => [
           $this->t('Project'),
           $this->t('Status'),
-          $this->t('Available updates'),
-          $this->t('Operations'),
         ],
-        '#rows' => $contrib,
+        '#rows' => $deprecated,
+      ],
+    ];;
+    $projectsDisplay['not_deprecated'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Found @project project with no known drupal 9 compatibility errors', ['@project' => count($notDeprecated)]),
+      '#tree' => TRUE,
+      '#open' => !empty($notDeprecated) ? TRUE : FALSE,
+      'data' => [
+        '#type' => 'table',
+        '#header' => [
+          $this->t('Project'),
+        ],
+        '#rows' => $notDeprecated,
+      ],
+    ];
+    $projectsDisplay['not_scanned'] = [
+      '#type' => 'details',
+      '#title' => $this->t('@project project not yet scanned', ['@project' => count($notScanned)]),
+      '#tree' => TRUE,
+      '#open' => FALSE,
+      'data' => [
+        '#type' => 'table',
+        '#header' => [
+          $this->t('Project'),
+        ],
+        '#rows' => $notScanned,
       ],
     ];
 
-    return $content;
+    return $projectsDisplay;
   }
 
 }
