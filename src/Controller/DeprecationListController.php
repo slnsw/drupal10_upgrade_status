f306aedcd78bc18817009da8504df643fb71e07c
Gábor Hojtsy <gabor@hojtsy.hu>
2019-04-02 16:11:45 +0200

    Shorten paths in popup and allow it to wrap. Highlight filename. Also allow error to wrap and implement linking on function names as a sample.

--- src/Controller/DeprecationListController.php
+++ src/Controller/DeprecationListController.php
@@ -90,11 +90,34 @@ class DeprecationListController extends ControllerBase {
 
     foreach ($report['files'] as $filepath => $errors) {
       foreach ($errors['messages'] as $error) {
+
+        // Remove the Drupal root directory and allow paths and namespaces to wrap.
+        // Emphasize filename as it may show up in the middle of the info.
+        $short_path = str_replace(DRUPAL_ROOT . '/', '', $filepath);
+        $short_path = str_replace('/', '&#8203;/&#8203;', $short_path);
+        if (strpos($short_path, 'in context of')) {
+          $short_path = preg_replace('!/([^/]+)( \(in context of)!', '/<strong>\1</strong>\2', $short_path);
+          $short_path = str_replace('\\', '&#8203;\\&#8203;', $short_path);
+        }
+        else {
+          $short_path = preg_replace('!/([^/]+)$!', '/<strong>\1</strong>', $short_path);
+        }
+
+        // @todo could be more accurate with reflection but not sure it is even possible as the reflected
+        //   code may not be in the runtime at this point (eg. functions in include files)
+        //   see https://www.php.net/manual/en/reflectionfunctionabstract.getfilename.php
+        //   see https://www.php.net/manual/en/reflectionclass.getfilename.php
+
+        // Link to documentation for a function in this specific Drupal version.
+        $api_version = preg_replace('!^(8\.\d+)\..+$!', '\1', \Drupal::VERSION);
+        $api_link = 'https://api.drupal.org/api/drupal/' . $api_version . '.x/search/';
+        $wrap_message = str_replace('\\', '&#8203;\\&#8203;', $error['message']);
+        $wrap_message = preg_replace('!deprecated function ([^(]+)\(\)!', 'deprecated function <a target="_blank" href="' . $api_link . '\1">\1()</a>', $wrap_message);
+
         $table[] = [
-          // @todo shorten filename for better display
           'filename' => [
             '#type' => 'markup',
-            '#markup' => $filepath,
+            '#markup' => $short_path,
           ],
           'line' => [
             '#type' => 'markup',
@@ -102,10 +125,7 @@ class DeprecationListController extends ControllerBase {
           ],
           'issue' => [
             '#type' => 'markup',
-            // @todo figure out how to link known components with reflection
-            // see https://www.php.net/manual/en/reflectionfunctionabstract.getfilename.php
-            // see https://www.php.net/manual/en/reflectionclass.getfilename.php
-            '#markup' => $error['message'],
+            '#markup' => $wrap_message,
           ],
         ];
       }
@@ -114,11 +134,7 @@ class DeprecationListController extends ControllerBase {
     // @todo print human readable name and version
     // @todo include action button to export once/if available
     return [
-      '#title' => $this->t('@project_name - detected issues (@count)', ['@project_name' => $project_name, '@count' => $project_error_count]),
-      'description' => [
-        '#type' => 'markup',
-        '#markup' => $this->t('All known Drupal 9 compatibility errors in this project are listed below.'),
-      ],
+      '#title' => $this->formatPlural($project_error_count, '@count known Drupal 9 error found in @project_name', '@count known Drupal 9 errors found in @project_name', ['@project_name' => $project_name]),
       'data' => $table,
     ];
   }
