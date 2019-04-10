8e811881f27f153458e224e89891f6ce0cbc63b1
Zoltán Herczog <zoltan.herczog@cheppers.com>
2019-04-10 13:45:20 +0200

    Add primary class to export report button on error report modal popup

--- src/Controller/DeprecationListController.php
+++ src/Controller/DeprecationListController.php
@@ -201,6 +201,7 @@ class DeprecationListController extends ControllerBase {
         '#attributes' => [
           'class' => [
             'button',
+            'button--primary',
           ],
         ],
       ],
