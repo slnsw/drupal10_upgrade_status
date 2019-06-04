698fc97457f996d984240b7661b074a11ec4e8e5
Gábor Hojtsy <gabor@hojtsy.hu>
2019-06-04 11:31:43 +0200

    Followup fix to refactoring of getItem to return an object like other item handlers.

--- src/Queue/InspectableQueue.php
+++ src/Queue/InspectableQueue.php
@@ -14,11 +14,14 @@ class InspectableQueue extends DatabaseQueue {
    */
   public function createItem($data) {
     $try_again = FALSE;
-    $job = $this->getItem($data);
 
-    if ($job && array_key_exists('item_id', $job)) {
-      return $job['item_id'];
+    // Check if this job already exists in the queue. Do not add the job again
+    // if it is already in this queue. Just return the existing job identifier.
+    $job = $this->getItem($data);
+    if ($job && !empty($job->item_id)) {
+      return $job->item_id;
     }
+
     try {
       $id = $this->doCreateItem($data);
     }
@@ -37,6 +40,16 @@ class InspectableQueue extends DatabaseQueue {
     return $id;
   }
 
+  /**
+   * Get an item from the queue based on the extension data.
+   *
+   * @param \Drupal\Core\Extension\Extension $extension
+   *   The extension to search for in the queue.
+   *
+   * @return bool|object
+   *   Returns false if the item is not in the queue currently. Returns the
+   *   item object if it is in the queue.
+   */
   public function getItem(Extension $data) {
     $path = $data->getPath();
     try {
