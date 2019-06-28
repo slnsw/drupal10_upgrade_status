a5b22bdf135c04bc3d28bff0e1b0a3f80bb046a1
Gábor Hojtsy <gabor@hojtsy.hu>
2019-06-28 15:31:43 +0200

    Fix tests for changes in the code and test coverage. Whoops.

--- tests/src/Functional/UpgradeStatusAnalyseTest.php
+++ tests/src/Functional/UpgradeStatusAnalyseTest.php
@@ -33,11 +33,15 @@ class UpgradeStatusAnalyseTest extends UpgradeStatusTestBase {
     $project = $key_value->get('upgrade_status_test_error');
     $this->assertNotEmpty($project);
     $report = json_decode($project, TRUE);
-    $this->assertEquals(1, $report['data']['totals']['file_errors']);
-    $this->assertCount(1, $report['data']['files']);
+    $this->assertEquals(2, $report['data']['totals']['file_errors']);
+    $this->assertCount(2, $report['data']['files']);
     $file = reset($report['data']['files']);
     $message = $file['messages'][0];
-    $this->assertEquals("Call to deprecated function menu_cache_clear_all():\nin Drupal 8.6.0, will be removed before Drupal 9.0.0. Use\n\Drupal::cache('menu')->invalidateAll() instead.", $message['message']);
+    $this->assertEquals("Syntax error, unexpected T_STRING on line 3", $message['message']);
+    $this->assertEquals(3, $message['line']);
+    $file = next($report['data']['files']);
+    $message = $file['messages'][0];
+    $this->assertEquals("Call to deprecated function menu_cache_clear_all(). Deprecated in Drupal 8.6.0, will be removed before Drupal 9.0.0. Use\n\Drupal::cache('menu')->invalidateAll() instead.", $message['message']);
     $this->assertEquals(10, $message['line']);
 
     $project = $key_value->get('upgrade_status_test_no_error');
@@ -53,7 +57,7 @@ class UpgradeStatusAnalyseTest extends UpgradeStatusTestBase {
     $this->assertCount(1, $report['data']['files']);
     $file = reset($report['data']['files']);
     $message = $file['messages'][0];
-    $this->assertEquals("Call to deprecated function format_string():\nin Drupal 8.0.0, will be removed before Drupal 9.0.0.\nUse \Drupal\Component\Render\FormattableMarkup.", $message['message']);
+    $this->assertEquals("Call to deprecated function format_string(). Deprecated in Drupal 8.0.0, will be removed before Drupal 9.0.0.\nUse \Drupal\Component\Render\FormattableMarkup.", $message['message']);
     $this->assertEquals(15, $message['line']);
   }
 
