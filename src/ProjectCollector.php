8266a39f45b96ef386fbf2880f5ccf04d13e5512
Gábor Hojtsy <gabor@hojtsy.hu>
2022-02-02 20:56:00 +0100

    Issue #3261167 by Liam Morland: Do not report disabled modules in collectCoreDeprecatedAndObsoleteExtensions()

--- src/ProjectCollector.php
+++ src/ProjectCollector.php
@@ -363,7 +363,7 @@ class ProjectCollector {
 
     /** @var \Drupal\Core\Extension\Extension $extension */
     foreach ($extensions as $key => $extension) {
-      if ($extension->origin === 'core' && !empty($extension->info['lifecycle']) && in_array($extension->info['lifecycle'], ['deprecated', 'obsolete'])) {
+      if (!empty($extension->status) && $extension->origin === 'core' && !empty($extension->info['lifecycle']) && in_array($extension->info['lifecycle'], ['deprecated', 'obsolete'])) {
         $prefix = '';
         $suffix = '';
         if (isset($extension->info['lifecycle_link'])) {
